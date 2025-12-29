fbb7bb6300c86fa3a21b035db0258bb28ae614a0
Ansh366239 <tandonansh5555@gmail.com>
2025-12-29 22:36:59 +0530

    added src folder

---
+++ src/main/java/com/example/coursecrud/entity/Event.java
@@ -0,0 +1,60 @@
+package com.example.coursecrud.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private String date;
+
+    // Default constructor (IMPORTANT)
+    public Event() {
+    }
+
+    // Parameterized constructor
+    public Event(Long id, String name, String location, String date) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.date = date;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/example/coursecrud/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.coursecrud.repository;
+
+import com.example.coursecrud.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
